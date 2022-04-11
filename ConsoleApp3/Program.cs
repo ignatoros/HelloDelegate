ea97946787ed782b1743e2e19b2652394a2f20c5
wsr10 <wsr10@K218PC11>
2022-04-11 11:29:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{ 
+    public delegate void HelloDelegate();
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloDelegate hD = () => Console.WriteLine("Hello");
+            hD();
+            Console.ReadKey();
+        }
+    }
+}
